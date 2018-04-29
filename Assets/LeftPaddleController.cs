cfbd690c9db14375bcf04add79f855d7d9dce349
uenio <hzhao1997@gmail.com>
2018-04-29 01:04:23 -0400

    ADDED LEAP MOTION SUPPORT. First hand is left player, second hand is right player. Make sure to add Core Module and Interaction Engine for Leap Motion.

---
+++ Assets/LeftPaddleController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+public class LeftPaddleController : MonoBehaviour {
+
+	Controller controller;
+	private float inital = 250;
+	private float zero_window = 50;
+	public float speed = 10.0f;
+	public float boundY = 2.25f;
+	private Rigidbody2D rb2d;
+
+	void Start()
+	{
+		controller = new Controller ();
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		Frame frame = controller.Frame ();
+
+		if (frame.Hands.Count > 0) 
+		{
+			Hand hand = frame.Hands [0];
+
+			List<Finger> finger = hand.Fingers;
+			Finger indexFinger = finger [1];
+			if(indexFinger.IsExtended)
+			{
+				Debug.Log (" Here are your current frames!" + finger[1].TipPosition.y);
+				var vel = rb2d.velocity;
+
+				if (indexFinger.TipPosition.y > inital+zero_window) {
+					vel.y = speed;
+				} else if (indexFinger.TipPosition.y < inital-zero_window) {
+					vel.y = -speed;
+				} else {
+					vel.y = 0;
+				}
+
+				rb2d.velocity = vel;
+
+					
+			}
+				
+
+
+			//Finger finger = hand.finger (1);
+
+		}
+
+		var pos = transform.position;
+		if (pos.y > boundY) {
+			pos.y = boundY;
+		} else if (pos.y < -boundY) {
+			pos.y = -boundY;
+		}
+		transform.position = pos;
+	}
+
+
+}
---
+++ Assets/RightPaddleController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+public class RightPaddleController : MonoBehaviour {
+
+	Controller controller;
+	private float inital = 250;
+
+	public float speed = 0.1f;
+	public float maxSpeed = 10.0f;
+	private float zero_window = 50;
+	public float boundY = 2.25f;
+	private Rigidbody2D rb2d;
+	private int frames = 0;
+
+	public Transform ball;
+
+	void Start()
+	{
+		controller = new Controller ();
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		Frame frame = controller.Frame ();
+		var pos = transform.position;
+
+
+		if (frame.Hands.Count > 1) {
+			Hand hand = frame.Hands [1];
+
+			List<Finger> finger = hand.Fingers;
+			Finger indexFinger = finger [1];
+			if (indexFinger.IsExtended) {
+				Debug.Log (" Here are your current frames!" + finger [1].TipPosition.y);
+				var vel = rb2d.velocity;
+
+				if (indexFinger.TipPosition.y > inital + zero_window) {
+					vel.y = maxSpeed;
+				} else if (indexFinger.TipPosition.y < inital - zero_window) {
+					vel.y = -maxSpeed;
+				} else {
+					vel.y = 0;
+				}
+
+				rb2d.velocity = vel;
+
+
+			}
+
+
+
+			//Finger finger = hand.finger (1);
+
+		} else {
+			/*
+			frames++;
+			if (frames % 7 == 0)
+			{
+				var vel = rb2d.velocity;
+				var ballpos = ball.position;
+
+				if (ballpos.y > pos.y)
+				{
+					vel.y += speed;
+					if (vel.y > maxSpeed)
+					{
+						vel.y = maxSpeed;
+					}
+				}
+				else if (ballpos.y < pos.y)
+				{
+					vel.y -= speed;
+					if (vel.y > -maxSpeed)
+					{
+						vel.y = -maxSpeed;
+					}
+				}
+					
+				rb2d.velocity = vel;
+			}
+		*/
+		}
+		if (pos.y > boundY) {
+			pos.y = boundY;
+		} else if (pos.y < -boundY) {
+			pos.y = -boundY;
+		}
+		transform.position = pos;
+	}
+
+
+}
